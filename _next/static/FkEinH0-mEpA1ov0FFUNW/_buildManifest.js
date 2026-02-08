43f202ea232998528241bc24ad2961299d6b7d71
FennJessica-FS <JLFenn@student.fullsail.edu>
2026-02-08 11:39:42 -0500

    Updates

---
+++ _next/static/FkEinH0-mEpA1ov0FFUNW/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/static-deploy-jess//_next/:path+",
+        "destination": "/static-deploy-jess/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
